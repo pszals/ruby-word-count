b0cbe7e20b6acb88fd36d4423000403fd2bcbd34
Philip Szalwinski <pszalwinski@gmail.com>
2023-04-05 17:21:07 -0600

    First test passes

--- word_count.rb
+++ word_count.rb
@@ -5,4 +5,12 @@ Write your code for the 'Word Count' exercise in this file. Make the tests in
 To get started with TDD, see the `README.md` file in your
 `ruby/word-count` directory.
 =end
+class Phrase
+  def initialize(string)
+    @string = string
+  end
 
+  def word_count
+    {"word"=>1}
+  end
+end
