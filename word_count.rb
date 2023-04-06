0ee5f3b839a3d27aa3682e25bd03d97c833d0cde
Philip Szalwinski <pszalwinski@gmail.com>
2023-04-05 18:08:24 -0600

    Pass expanded lists and punctuation using regex for punctuation

--- word_count.rb
+++ word_count.rb
@@ -6,17 +6,23 @@ To get started with TDD, see the `README.md` file in your
 `ruby/word-count` directory.
 =end
 class Phrase
+  PUNCTUATION_REGEX = /!|@|&|\$|%|\^|:/
   def initialize(string)
     @string = string
   end
 
   def word_count
-    words = @string.split(/\W+/)
+    words = @string.split(/\s|,/)
     words.each_with_object({}) do |word, obj|
-      if obj[word].nil?
-        obj[word] = 1
-      elsif
-        obj[word] += 1
+      word.downcase!
+      word.strip!
+      word.gsub!(PUNCTUATION_REGEX, "")
+      if !word.empty?
+        if obj[word].nil?
+          obj[word] = 1
+        elsif
+          obj[word] += 1
+        end
       end
     end
   end
