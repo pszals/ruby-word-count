11ecab8df3fa79a6dd0638bac20dee50ec6b8a13
Philip Szalwinski <pszalwinski@gmail.com>
2023-04-05 18:26:59 -0600

    Update spacing

--- word_count.rb
+++ word_count.rb
@@ -1,13 +1,8 @@
-=begin
-Write your code for the 'Word Count' exercise in this file. Make the tests in
-`word_count_test.rb` pass.
-
-To get started with TDD, see the `README.md` file in your
-`ruby/word-count` directory.
-=end
 class Phrase
+
   PUNCTUATION_REGEX = /!|@|&|\$|%|\^|:|\./
   QUOTES_TO_TRIM = /\A'|'\z/
+
   def initialize(string)
     @string = string
   end
@@ -19,10 +14,11 @@ class Phrase
       word.strip!
       word.gsub!(PUNCTUATION_REGEX, "")
       word.gsub!(QUOTES_TO_TRIM, "")
+
       if !word.empty?
         if obj[word].nil?
           obj[word] = 1
-        elsif
+        else
           obj[word] += 1
         end
       end
